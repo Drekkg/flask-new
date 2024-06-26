e9e4ad67d36448be1bd112c47ed1ef327f770718
drekkg <rizzagum@gmail.com>
2024-06-27 00:12:39 +0200

    fix req

--- run.py
+++ run.py
@@ -49,7 +49,7 @@ def careers():
 if __name__ == "__main__":
     app.run(
         host = os.environ.get("IP", "0.0.0.0"),
-        port = int(os.environ.get("port", "5000")),
+        port = int(os.environ.get("PORT", 5000)),
         debug = True)
     
     
